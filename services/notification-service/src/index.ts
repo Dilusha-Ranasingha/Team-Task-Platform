42cf00d86230e8066006e83c37d6e14acb15c799
NDilusha <161825980+Dilusha-Ranasingha@users.noreply.github.com>
2025-11-21 12:40:52 +0530

    feat: add notification service with MongoDB connection and basic server setup
    ✔ Creating notification-service folder
    ✔ Installing dependencies
    ✔ Creating TypeScript config
    ✔ Creating architecture folders
    ✔ Creating mongoose connection
    ✔ Creating index.ts
    ✔ Setting up .env
    ✔ Running the service on port 3003

---
+++ services/notification-service/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./infrastructure/db/mongoose";
+//import notificationRoutes from "./presentation/routes/notificationRoutes";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+//app.use("/notifications", notificationRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Notification Service Running");
+});
+
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(3003, () => {
+    console.log("Notification Service running on port 3003");
+  });
+};
+
+startServer();
